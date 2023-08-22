16b7dcd906a3dec2a1946ebd656d7ed0d9980771
Raboro <marius.woerfelreal@gmail.com>
2023-08-22 21:51:07 +0200

    added dateFormatting to settings

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,18 +1,67 @@
-import { PluginSettingTab } from 'obsidian';
+import { App, Notice, PluginSettingTab, Setting } from 'obsidian';
+import ExpirationDateTrackerPlugin from 'src/main';
 
 export interface ExpirationDateTrackerSettings {
-    // insert settings
-    example: string;
+    dateFormatting: string;
 }
 
 export const DEFAULT_SETTINGS: ExpirationDateTrackerSettings = {
-    example: ''
+    dateFormatting: 'DD/MM/YYYY'
 }; 
 
 export class ExpirationDateTrackerSettingsTab extends PluginSettingTab {
-    
-    display() {
+    plugin: ExpirationDateTrackerPlugin;
 
+	constructor(app: App, plugin: ExpirationDateTrackerPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+    
+    display(): void {
+        this.containerEl.empty();
+        this.dateFormattingSettings();
     }
+
+    dateFormattingSettings(): Setting {
+        return new Setting(this.containerEl)
+        .setName('Date formatting')
+        .setDesc('Format your dates will be displayed and collected')
+        .addText(text => text
+            .setPlaceholder('Enter your format')
+            .setValue(this.plugin.settings.dateFormatting)
+            .onChange(async value => await this.dateFormattingSettingsOnChange(value)));
+    }
+
+    dateFormattingSettingsOnChange = async (value: string) => {
+        let noticeMessage = 'Wrong date formatting!!';
+        if (this.isFormattingValid(value)) {
+            this.plugin.settings.dateFormatting = value;
+            await this.plugin.saveSettings();
+            noticeMessage = 'Valid date formatting';
+        } 
+        new Notice(noticeMessage);
+    };
+
+	isFormattingValid(format: string): boolean {
+		const containsDoubleD: boolean = this.formatContains('DD', format);
+		const containsDoubleM: boolean = this.formatContains('MM', format);
+		const containsFourY: boolean = this.formatContains('YYYY', format);
+		return containsDoubleD && containsDoubleM && containsFourY && !this.containsInvalidChars(format);
+	}
+
+	formatContains(subStr: string, format: string): boolean {
+		return format.contains(subStr) || format.contains(subStr.toLowerCase());
+	}
+
+	containsInvalidChars(format: string): boolean {
+		const invalidChars: string[] = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 
+										'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z'];
+		for (const invalidChar in invalidChars) {
+			if (this.formatContains(invalidChar, format)) {
+				return true;
+			}
+		}
+		return false;
+	}
     
 }
