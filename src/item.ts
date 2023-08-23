47d6e4814083a980e0c9f1360e280d02ad45c11a
Raboro <marius.woerfelreal@gmail.com>
2023-08-23 22:13:03 +0200

    added code to handle empty numberOfElements -> 1

--- src/item.ts
+++ src/item.ts
@@ -5,9 +5,16 @@ export default class Item {
     private expireDate: ExpirationDate;
     private numberOfElements: string | number | undefined;
 
-    constructor(name: string, expireDate: ExpirationDate, numberOfElements?: string | number) {
+    constructor(name: string, expireDate: ExpirationDate, numberOfElements: string | number | undefined) {
         this.name = name;
         this.expireDate = expireDate;
-        this.numberOfElements = numberOfElements ?? undefined;
+        this.numberOfElements = this.isEmpty(numberOfElements) ? 1 : numberOfElements;
+    }
+
+    private isEmpty(numberOfElements: string | number | undefined): boolean {
+        if (numberOfElements && numberOfElements.toString() === '-') {
+            return true;
+        }
+        return false;
     }
 }
