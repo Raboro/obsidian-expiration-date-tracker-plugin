d86d2067e4f6974b82ff53a6a508967665738ba3
Raboro <marius.woerfelreal@gmail.com>
2023-08-23 22:22:28 +0200

    collected items of table

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Notice, Plugin, TFile } from 'obsidian';
 import { DEFAULT_SETTINGS, ExpirationDateTrackerSettings, ExpirationDateTrackerSettingsTab } from './settings/settings';
+import Item from './item';
+import ExpirationDate from './expirationDate';
 
 
 export default class ExpirationDateTrackerPlugin extends Plugin {
@@ -37,7 +39,30 @@ export default class ExpirationDateTrackerPlugin extends Plugin {
     }
 
     trackExpirationDatesOfContent(content: string): void {
+        this.collectItems(this.splitContent(content));
+    }
 
+    splitContent(content: string): Array<string> {
+        const splittedContent = content.split('|');
+        splittedContent.remove(' \n');
+        splittedContent.remove(' --- ');
+        splittedContent.remove('');
+        return splittedContent;
+    }
+
+    collectItems(content: Array<string>): Array<Item> {
+        const items: Array<Item> = [];
+        for (let i = 1; i < (content.length / 3); i++) {
+            items.push(this.createItem(content, i));
+        }
+        return items;
+    }
+
+    createItem(content: Array<string>, i: number): Item {
+        const name = content[0+3*i].trim();
+        const date = content[1+3*i].trim();
+        const numberOfElements = content[2+3*i] === undefined ? undefined : content[2+3*i].trim();
+        return new Item(name, new ExpirationDate(date, this.settings.dateFormatting), numberOfElements);
     }
 
     async loadSettings() {
