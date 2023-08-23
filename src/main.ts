cabce8e1444c94c1679cf7656447f72a6c52ec11
Raboro <marius.woerfelreal@gmail.com>
2023-08-23 21:17:53 +0200

    added content fetching

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin, TFile } from 'obsidian';
 import { DEFAULT_SETTINGS, ExpirationDateTrackerSettings, ExpirationDateTrackerSettingsTab } from './settings/settings';
 
 
@@ -19,8 +19,17 @@ export default class ExpirationDateTrackerPlugin extends Plugin {
     }
 
     trackExpirationDates = async () => {
-        
+       const content = await this.fetchContent(); 
     };
+
+    async fetchContent(): Promise<string | undefined> {
+        const file = this.app.vault.getAbstractFileByPath(this.settings.expirationDateNodeLocation);
+		if (file && file instanceof TFile) {
+			return (await this.app.vault.read(file)).trim();
+		}
+		new Notice('Node could not be found at location: ' + this.settings.expirationDateNodeLocation);
+		return undefined;
+    }
 
     async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
