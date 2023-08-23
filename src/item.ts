182013d1c1f5b644c9c9cfc7a8a2e6aee79edbda
Raboro <marius.woerfelreal@gmail.com>
2023-08-24 00:59:20 +0200

    renamed variable

--- src/item.ts
+++ src/item.ts
@@ -5,17 +5,17 @@ export type NumberOfElements = string | number | undefined;
 
 export default class Item {
     private name: string;
-    private expireDate: ExpirationDate;
+    private expirationDate: ExpirationDate;
     private numberOfElements: NumberOfElements;
     private expirationCategory: ExpirationCategory;
 
-    constructor(name: string, expireDate: ExpirationDate, numberOfElements: NumberOfElements, 
+    constructor(name: string, expirationDate: ExpirationDate, numberOfElements: NumberOfElements, 
         expirationCategories: ExpirationCategory[]) {
         this.name = name;
-        this.expireDate = expireDate;
+        this.expirationDate = expirationDate;
         this.numberOfElements = this.isEmpty(numberOfElements) ? 1 : numberOfElements;
         expirationCategories.forEach(c => {
-            if (c.isCategory(this.expireDate.getDaysTillExpired())) {
+            if (c.isCategory(this.expirationDate.getDaysTillExpired())) {
                 this.expirationCategory = c;
             }
         });
