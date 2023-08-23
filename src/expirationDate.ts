f032b9cfdf1ba4a95cc473707bb5613e7c60529d
Raboro <marius.woerfelreal@gmail.com>
2023-08-23 21:33:40 +0200

    added ExpirationDate with basic date calculation

---
+++ src/expirationDate.ts
@@ -0,0 +1,31 @@
+export default class ExpirationDate {
+    private str: string;
+    private date: Date;
+
+    constructor(str: string, dateFormatting: string) {
+		this.str = str;
+        this.convertStringToDate(dateFormatting);
+	}
+
+    private convertStringToDate(dateFormatting: string) {
+        this.date = this.constructDate(
+            dateFormatting.search('DD'),
+            dateFormatting.search('MM'),
+            dateFormatting.search('YYYY')
+        );
+    }
+
+    private constructDate(dayIndex: number, monthIndex: number, yearIndex: number): Date {
+        const date = new Date();
+        date.setFullYear(
+            this.dateNumber(yearIndex, yearIndex+4), 
+            this.dateNumber(monthIndex, monthIndex+2, 1), // month has one offset
+            this.dateNumber(dayIndex, dayIndex+2)
+        );
+        return date;
+    }
+
+    private dateNumber(start: number, end: number, offset?: number): number {
+        return parseInt(this.str.substring(start, end)) - (offset ?? 0);
+    }
+}
