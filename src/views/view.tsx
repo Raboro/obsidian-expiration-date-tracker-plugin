8dd9258b6a38a37e30af6c275b07531b57f68919
Raboro <marius.woerfelreal@gmail.com>
2023-08-24 16:39:41 +0200

    used component in view

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -1,10 +1,14 @@
 import { ItemView } from 'obsidian';
 import Item from 'src/item';
+import * as React from 'react';
+import ItemUi from 'src/ui/itemUi';
+import { Root, createRoot } from 'react-dom/client';
 
 export const EXPIRATION_DATE_TRACKER_VIEW_TYPE = 'Expiration-Date-Tracker';
 
 export class ExpirationDateTrackerView extends ItemView {
     private container: HTMLDivElement;
+    private root: Root;
     icon = 'timer';
 
     getViewType(): string {
@@ -18,14 +22,22 @@ export class ExpirationDateTrackerView extends ItemView {
     async onOpen(): Promise<void> {
         const { contentEl } = this;
         contentEl.createEl('h1', {text: 'Expiration Date Tracker'});  
-        this.container = contentEl.createDiv({cls: 'itemsFlexboxContainer'});  
+        this.container = contentEl.createDiv({cls: 'itemsFlexboxContainer'});
+    }
+    
+    displayItems(items: Item[]): void {
+        if (!this.root) {
+            this.root = createRoot(this.container);
+        }
+        this.root.render(
+            <React.StrictMode>
+                <ItemUi />
+            </React.StrictMode>
+        );
     }
 
-    displayItems(items: Item[]): void {
-        while (this.container.firstChild) {
-            this.container.removeChild(this.container.lastChild as Node);
-        } 
-        // handle items
+    onclose(): void {
+        this.root.unmount();
     }
 
 }
