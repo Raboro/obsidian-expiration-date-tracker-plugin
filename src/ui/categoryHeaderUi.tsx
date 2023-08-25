1e2be362f89c9c0e9501875cb3575f81bd435d8a
Raboro <marius.woerfelreal@gmail.com>
2023-08-26 00:23:11 +0200

    removed empty line & added semicolon

--- src/ui/categoryHeaderUi.tsx
+++ src/ui/categoryHeaderUi.tsx
@@ -5,12 +5,11 @@ export interface CategoryHeader {
 }
 
 export default function CategoryHeaderUi({ name, itemCounter, changeDisplay }: CategoryHeader) {
-
     return <>
             <div className="categoryHeaderContainer">
                 <h4>{name}</h4>
                 <h4>{itemCounter}</h4>
                 <a onClick={changeDisplay} className="expandIcon">Click</a>
             </div>
-        </>
+        </>;
 }
