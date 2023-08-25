b05f421942a43d7014401dd3ee2a8f6470de9c86
Raboro <marius.woerfelreal@gmail.com>
2023-08-26 00:19:49 +0200

    added category component

---
+++ src/ui/categoryUi.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import ItemUi from "./itemUi";
+import CategoryHeaderUi from "./categoryHeaderUi";
+import Item from "src/item";
+
+export interface ICategoryUi {
+    name: string;
+    items: Item[];
+}
+
+export default function CategoryUi({ name, items }: ICategoryUi) {
+    const [contentDisplay, setContentDisplay] = useState(false);
+
+    const changeDisplay = () => {
+        setContentDisplay(prevDisplay => !prevDisplay);
+    };
+    
+    return (
+        <div className='categoryContainer'>
+            <CategoryHeaderUi name={name} itemCounter={items.length} changeDisplay={changeDisplay} />
+            {contentDisplay && (
+                <div className='itemsFlexboxContainer'>
+                    {items.map(item => {
+                        const { name, expirationDate, numberOfElements, expirationCategory } = item.toDTO();
+                        return (
+                            <ItemUi
+                                key={uuidv4()}
+                                name={name}
+                                expirationDate={expirationDate}
+                                numberOfElements={numberOfElements}
+                                expirationCategory={expirationCategory}
+                            />
+                        );
+                    })}
+                </div>
+            )}
+        </div>
+    );
+}
