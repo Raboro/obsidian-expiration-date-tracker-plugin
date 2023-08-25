dde2c5f793688e7d8fd0bbb6ed54ca617ac293af
Raboro <marius.woerfelreal@gmail.com>
2023-08-26 00:24:19 +0200

    used single quote

--- src/ui/categoryUi.tsx
+++ src/ui/categoryUi.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import ItemUi from "./itemUi";
-import CategoryHeaderUi from "./categoryHeaderUi";
-import Item from "src/item";
+import ItemUi from './itemUi';
+import CategoryHeaderUi from './categoryHeaderUi';
+import Item from 'src/item';
 
 export interface ICategoryUi {
     name: string;
