6c1cc50f83fdb459b5fad820e62d63781ab8de86
Raboro <marius.woerfelreal@gmail.com>
2023-08-26 00:18:58 +0200

    added CategoryHeader component

---
+++ src/ui/categoryHeaderUi.tsx
@@ -0,0 +1,16 @@
+export interface CategoryHeader {
+    name: string;
+    itemCounter: number;
+    changeDisplay: () => void;
+}
+
+export default function CategoryHeaderUi({ name, itemCounter, changeDisplay }: CategoryHeader) {
+
+    return <>
+            <div className="categoryHeaderContainer">
+                <h4>{name}</h4>
+                <h4>{itemCounter}</h4>
+                <a onClick={changeDisplay} className="expandIcon">Click</a>
+            </div>
+        </>
+}
