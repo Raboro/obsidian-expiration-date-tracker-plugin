2596b6b7fbf8983160137a9ec8e5ab55943958c6
Raboro <marius.woerfelreal@gmail.com>
2023-08-26 13:20:07 +0200

    replaced h2 with p & used styles

--- src/ui/itemUi.tsx
+++ src/ui/itemUi.tsx
@@ -2,10 +2,10 @@ import { ItemDTO } from 'src/item';
 
 export default function ItemUi({ name, expirationDate, numberOfElements, expirationCategory }: ItemDTO) {
     return <>
-            <div style={{border: '1px solid black', display: 'flex', flexDirection: 'column'}}>
-                <h2>{name}</h2>
-                <h2>{expirationDate.getDaysTillExpired()}</h2>
-                <h2>{numberOfElements}</h2>
+            <div className='itemContainer'>
+                <p>{name}</p>
+                <p>{numberOfElements}</p>
+                <p>{expirationDate.getDaysTillExpired()}</p>
             </div>        
         </>;
 }
