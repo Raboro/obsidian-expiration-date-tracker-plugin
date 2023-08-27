b25d4004e315c0eadffb86786319c46e68b507a3
Raboro <raborogit@gmail.com>
2023-08-27 17:43:23 +0200

    made var private & used .some instead of for loop

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -25,7 +25,7 @@ export const DEFAULT_SETTINGS: ExpirationDateTrackerSettings = {
 }; 
 
 export class ExpirationDateTrackerSettingsTab extends PluginSettingTab {
-    plugin: ExpirationDateTrackerPlugin;
+    private plugin: ExpirationDateTrackerPlugin;
 
 	constructor(app: App, plugin: ExpirationDateTrackerPlugin) {
 		super(app, plugin);
@@ -76,12 +76,7 @@ export class ExpirationDateTrackerSettingsTab extends PluginSettingTab {
 	containsInvalidChars(format: string): boolean {
 		const invalidChars: string[] = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 
 										'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', '|'];
-		for (const invalidChar in invalidChars) {
-			if (this.formatContains(invalidChar, format)) {
-				return true;
-			}
-		}
-		return false;
+		return invalidChars.some(invalidChar => this.formatContains(invalidChar, format));
 	}
 
     expirationDateNodeLocationSettings(): Setting {
