2ec17b857f052716e78f5b9018043d2be3babda0
Raboro <raborogit@gmail.com>
2023-08-27 13:32:23 +0200

    added searchSelectExpirationCategoryModal

---
+++ src/modals/searchSelectExpirationCategoryModal.ts
@@ -0,0 +1,27 @@
+import { App, FuzzySuggestModal, Notice } from "obsidian";
+import ExpirationCategory from "src/expirationCategory";
+import { ItemDTO } from "src/item";
+
+export default class SearchSelectExpirationCategoryModal extends FuzzySuggestModal<ExpirationCategory> {
+    private expirationCategories: ExpirationCategory[];
+    private items: ItemDTO[];
+
+    constructor(app: App, expirationCategories: ExpirationCategory[], items: ItemDTO[]) {
+        super(app);
+        this.expirationCategories = expirationCategories;
+        this.items = items;
+    }
+
+    getItems(): ExpirationCategory[] {
+        return this.expirationCategories;
+    }
+
+    getItemText(item: ExpirationCategory): string {
+        return item.getName();
+    }
+
+    onChooseItem(item: ExpirationCategory, evt: MouseEvent | KeyboardEvent): void {
+        new Notice(item.getName());
+    }
+
+}
